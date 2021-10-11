27d9b93eae581f0b0843a741997e507d58405e64
Victor Teo <victor.teo@fillmula.com>
2021-10-11 19:33:29 +0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='jsonclasses-orm',
-      version='2.8.1',
+      version='3.0.0',
       description='Definitions for JSONClasses ORM integration implementation.',
       long_description=README,
       long_description_content_type="text/markdown",
@@ -20,5 +20,5 @@ setup(name='jsonclasses-orm',
       zip_safe=False,
       url='https://github.com/fillmula/jsonclasses-orm',
       include_package_data=True,
-      python_requires='>=3.9',
+      python_requires='>=3.10',
       install_requires=['qsparser>=1.0.1,<2.0.0'])
